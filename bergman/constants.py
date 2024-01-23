5a4a5d6bb3ee59eb29d5a9ac1a9a06b797ebfdcb
Rajat Goyal <132493320+RAJAT-COD3@users.noreply.github.com>
2024-01-24 01:35:19 +0530

    Add files via upload

---
+++ bergman/constants.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Constant:
+    NUM_MEALS = 8
+    I = 18.7813
+    X = 0.0067
+    G = 120.0
+    t = 0.0
+    h = 1.0
+
+    p1 = 0.0337
+    p2 = 0.0209
+    p3 = 7.5 * pow(10, -6)
+    tau = 0.083333
+    n = 0.214
+    Gb = 144.0
+
+    u = 5.82
+
+    Ag = 0.8
+    tmax_I = 33.0
+    tmax_G = 24.0
+    Vg = 13.79
+
+    MAX_TIME = 1440
+
+
+
