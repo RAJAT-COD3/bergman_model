9b90c6b4aed5a14cc78c9fa7fdb8cf46677c32ea
Rajat Goyal <132493320+RAJAT-COD3@users.noreply.github.com>
2024-02-05 00:31:47 +0530

    Update constants.py code quality

--- bergman/constants.py
+++ bergman/constants.py
@@ -1,29 +1,23 @@
-from typing import List
+class Constants:
+    def __init__(self):
+        self.I = 0
+        self.X = 0.0067
+        self.G = 120.0
+        self.t = 0.0
+        self.h = 1.0
 
+        self.p1 = 0.0337
+        self.p2 = 0.0209
+        self.p3 = 7.5 * pow(10, -6)
+        self.tau = 0.083333
+        self.n = 0.214
+        self.Gb = 144.0
 
-class Constant:
+        self.u = 0.054
 
-    I = 18.7813
-    X = 0.0067
-    G = 120.0
-    t = 0.0
-    h = 1.0
+        self.Ag = 0.8
+        self.tmax_I = 33.0
+        self.tmax_G = 24.0
+        self.Vg = 13.79
 
-    p1 = 0.0337
-    p2 = 0.0209
-    p3 = 7.5 * pow(10, -6)
-    tau = 0.083333
-    n = 0.214
-    Gb = 144.0
-
-    u = 0.054
-
-    Ag = 0.8
-    tmax_I = 33.0
-    tmax_G = 24.0
-    Vg = 13.79
-
-    MAX_TIME = 1440
-
-
-
+        self.MAX_TIME = 1440
